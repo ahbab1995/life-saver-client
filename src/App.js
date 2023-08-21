50f7b357e9144885450e97df4c6c0976314fbebd
ahbab1995 <islamahbab1996@yahoo.com>
2023-08-21 19:08:44 +0600

    css framework install and plugging install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
