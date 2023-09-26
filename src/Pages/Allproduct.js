58beb52bec14957b1bf3e38f385578f7a07e40b0
ahbab1995 <islamahbab1996@yahoo.com>
2023-09-27 00:03:44 +0600

    all product data show code added

--- src/Pages/Allproduct.js
+++ src/Pages/Allproduct.js
@@ -1,11 +1,34 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import banner from "../assets/pexels.png.jpg";
 const Allproduct = () => {
-    return (
-        <div>
-            <h1>this is All Product Page</h1>
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div>
+      <div>
+        <div className="justify-items-center grid gap-5 grid-cols-1  md:grid-cols-2 lg:grid-cols-3 ">
+          {products.map((product) => (
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure className="px-10 pt-10">
+                <img src={banner} alt="Shoes" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">{product.name}</h2>
+                <p> {}</p>
+                <div className="card-actions justify-start">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Allproduct;
--- src/Pages/Home/Landing.js
+++ src/Pages/Home/Landing.js
@@ -1,52 +1,38 @@
 import React from "react";
 import banner from "../../assets/pexels.png.jpg";
 import { Link } from "react-router-dom";
-import { useEffect,useState } from "react";
-
+import { useEffect, useState } from "react";
 
 const Landing = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data));
-}, [])
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div>
       <h1 className="lg:text-5xl text-3xl my-16 text-center">
         Combinations for your adventures
       </h1>
-      {products.map(product=>
-      <div className="hero  ">
-        <div className="hero-content flex-col lg:flex-row">
-          <img src={banner} className=" rounded-lg " alt="banner"/>
-          <div>
-            <h1 className="text-5xl ">{product.name}</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <Link to='/allproduct' className="btn btn-primary">GO TO SHOP</Link>
-          </div>
+      <div>
+        <div className="justify-items-center grid gap-5 grid-cols-1  md:grid-cols-2 lg:grid-cols-3 ">
+          {products.map((product) => (
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure className="px-10 pt-10">
+                <img src={banner} alt="Shoes" className="rounded-xl" />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">{product.name}</h2>
+                <p> {}</p>
+                <div className="card-actions justify-start">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      )}
-      <div className="hero  ">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={banner} className=" rounded-lg " alt="banner"/>
-          <div>
-            <h1 className="text-5xl ">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">GO TO SHOP</button>
-          </div>
-        </div>
-      </div>
-      
     </div>
   );
 };
